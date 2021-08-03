5f8a968fa3b4c23e7f35f390f2ba7aee345c1202
Marina Monsivais <minamonsi@me.com>
2021-08-03 16:08:04 -0500

    Created server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// implement your API here
+// implement your API here ( make sure you have express in the dependencies.  Do an npm install express)
+
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) =>{ //request handler function
+    res.send('Hello Noe!');
+})
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee'
+        },
+        {
+            id:2,
+            name: 'Frodo Baggins'
+        }
+    ];
+
+    //200 means the request is ok
+    res.status(200).json(hobbits);
+})
+
+server.listen(8000, () => console.log('API Running on port 8000'));
+
+
+
